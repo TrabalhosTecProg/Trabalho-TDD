06a751596cf6a65bdbef8dc724fe4209290dd551
SwampTG <adriansoareslps@gmail.com>
2022-12-17 21:39:45 -0300

    Exceções de cadastro de rendimentos

---
+++ calculadoraIRPF/src/main/java/org/tppe/tp1/exceptions/CadastroInvalidoException.java
@@ -0,0 +1,7 @@
+package org.tppe.tp1.exceptions;
+
+public class CadastroInvalidoException extends Exception{
+    public CadastroInvalidoException(String message) {
+        super(message);
+    }
+}
