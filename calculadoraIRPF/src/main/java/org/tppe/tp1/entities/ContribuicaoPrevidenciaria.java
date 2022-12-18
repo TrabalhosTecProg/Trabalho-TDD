fdc7057c1c75324a631f3c5b987acaa401cd67ac
biancasofia <sofiabrasil44@gmail.com>
2022-12-18 10:40:53 -0300

    Criacao de contribuicao previdenciaria e teste
    Co-Authored-By: Lorrany Oliveira <lolo.souza80@yahoo.com>

---
+++ calculadoraIRPF/src/main/java/org/tppe/tp1/entities/ContribuicaoPrevidenciaria.java
@@ -0,0 +1,24 @@
+package org.tppe.tp1.entities;
+
+public class ContribuicaoPrevidenciaria {
+
+	private String descricao;	
+	private double valor ;
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+	
+	
+	
+}
