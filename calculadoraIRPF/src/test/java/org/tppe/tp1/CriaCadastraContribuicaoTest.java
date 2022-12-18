b84d88004bad26da1818d61ac9fbad4df444c561
biancasofia <sofiabrasil44@gmail.com>
2022-12-18 12:30:18 -0300

    Parametrizacao de testes de cadastroContribuicao
    Co-Authored-By: Lorrany Oliveira <lolo.souza80@yahoo.com>

--- calculadoraIRPF/src/test/java/org/tppe/tp1/CriaCadastraContribuicaoTest.java
+++ calculadoraIRPF/src/test/java/org/tppe/tp1/CriaCadastraContribuicaoTest.java
@@ -2,17 +2,65 @@ package org.tppe.tp1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.tppe.tp1.exceptions.CadastroInvalidoException;
 import org.tppe.tp1.usecases.CadastrarContribuicao;
 
-class CriaCadastraContribuicaoTest {
+@RunWith(Parameterized.class)
+public class CriaCadastraContribuicaoTest {
 
 	CadastrarContribuicao contribuicaoPrevidencia;
+	Object[][] contribuicoes;
+	double valoresCorretos;
 	
+	@Before
+	public void setup(){
+		contribuicaoPrevidencia= new CadastrarContribuicao();
+	}
+
+	public CriaCadastraContribuicaoTest(Object[][] contribuicoes, double valoresCorretos) {
+		this.contribuicoes= contribuicoes;
+		this.valoresCorretos= valoresCorretos;
+	}
 	
-	@Test
+	@Parameters
+    public static Collection<Object[]> getParametros() {
+        Object[][] valorEsperado= new Object[][] {
+        	{new Object[][] {
+        		{"INSS", 30.00}
+        		}, 30.00},
+        	{new Object[][] {
+        		{"INSS", 30.00},
+        		{"Previdencia Privada", 300.00}
+        		}, 330.00},
+        	{new Object[][] {
+        		{"INSS", 30.00},
+        		{"Previdencia Privada", 300.00},
+            	{"Contracheque", 50.00}
+            		}, 380.00}
+        };
+        	return Arrays.asList(valorEsperado);
+       
+    }
+
+    @Test
+     public void CadastroContribuicaoTest() {
+    	contribuicaoPrevidencia= new CadastrarContribuicao();
+    	for(Object[] c:contribuicoes) {
+    		//CadastrarContribuicao contribuicaoPrevidencia= new CadastrarContribuicao();
+    		contribuicaoPrevidencia.add((String)c[0], (double)c[1]);
+    	}
+    	assertEquals(valoresCorretos, contribuicaoPrevidencia.getTotalContribuicao(),0);
+    }
+	
+	/*@Test
 	void testCadastroUmaContribuicao() throws CadastroInvalidoException {
 		contribuicaoPrevidencia= new CadastrarContribuicao();
 		contribuicaoPrevidencia.add("INSS", 30.00);
@@ -35,7 +83,7 @@ class CriaCadastraContribuicaoTest {
 		contribuicaoPrevidencia.add("Previdencia Privada", 300.00);
 		contribuicaoPrevidencia.add("Contracheque", 50.00);
 		assertEquals(380.00, contribuicaoPrevidencia.getTotalContribuicao(), 0);
-	}
+	}*/
 	
 	
 }
