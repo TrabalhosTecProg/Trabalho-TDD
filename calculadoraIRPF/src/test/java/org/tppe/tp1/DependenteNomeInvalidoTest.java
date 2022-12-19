599a8ecde5c64f0b3e3a5f772c1b222f2f929434
biancasofia <sofiabrasil44@gmail.com>
2022-12-19 12:03:35 -0300

    Add cadastro de dependente e mais testes
    Co-Authored-By: Lorrany Oliveira  <lolo.souza80@yahoo.com>

--- calculadoraIRPF/src/test/java/org/tppe/tp1/DependenteNomeInvalidoTest.java
+++ calculadoraIRPF/src/test/java/org/tppe/tp1/DependenteNomeInvalidoTest.java
@@ -5,12 +5,16 @@ import org.junit.Test;
 import java.time.LocalDate;
 import org.tppe.tp1.entities.Dependente;
 import org.tppe.tp1.exceptions.NomeEmBrancoException;
+import org.tppe.tp1.usecases.CadastroDependente;
 
 public class DependenteNomeInvalidoTest {
 	private Dependente dependente;
+	private CadastroDependente cadastroDependentes;
+	
 	@Before
 	public void setup() {
 		this.dependente= new Dependente();
+		this.cadastroDependentes= new CadastroDependente();
 	}
 	
 	@Test(expected = NomeEmBrancoException.class)
@@ -23,5 +27,12 @@ public class DependenteNomeInvalidoTest {
         dependente.setNome(null); // esse falhou e na classe foi adicionada a cláusula de vazio
         dependente.setData(LocalDate.of(2020, 07, 22));
     }
+	// testar no cadastro
+		@Test(expected = NomeEmBrancoException.class)
+		public void NomeEmBrancoCadastro() throws NomeEmBrancoException{
+			cadastroDependentes.add(" ", LocalDate.of(2020, 07, 22));
+	      
+	    }
+	
 
 }
