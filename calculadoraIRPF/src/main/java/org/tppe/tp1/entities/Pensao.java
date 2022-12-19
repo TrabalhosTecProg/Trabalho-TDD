4ba2bc209fe90c4dc08936d5b00942585ab31855
lorranyoliveira <lolo.souza80@yahoo.com>
2022-12-18 23:30:41 -0300

    Adiciona teste de cadastro de pensão com falsificao.
    Co-Authored-By: Bianca Sofia <sofiabrasil44@gmail.com>

--- calculadoraIRPF/src/main/java/org/tppe/tp1/entities/Pensao.java
+++ calculadoraIRPF/src/main/java/org/tppe/tp1/entities/Pensao.java
@@ -1,7 +1,16 @@
 package org.tppe.tp1.entities;
 
 public class Pensao {
+
 	private double valor;
+	
+	public Pensao() {
+		super();
+	}
+	
+	public Pensao(double valor) {
+		this.valor = valor;
+	}
 	
 	public double getValor() {
 		return valor;
--- calculadoraIRPF/src/test/java/org/tppe/tp1/CriaCadastroPensaoTest.java
+++ calculadoraIRPF/src/test/java/org/tppe/tp1/CriaCadastroPensaoTest.java
@@ -3,12 +3,18 @@ package org.tppe.tp1;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.tppe.tp1.entities.Pensao;
+import org.tppe.tp1.usecases.CadastrarPensao;
 
 class CriaCadastroPensaoTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void cadastrarUmaPensao() {
+		CadastrarPensao cadastroPensao = new CadastrarPensao();
+		Pensao pensao = new Pensao();
+		pensao.setValor(1000.00D);
+		cadastroPensao.addPensao(pensao);
+		assertEquals(1000.00D, cadastroPensao.getTotalPensao());
 	}
 
 }
