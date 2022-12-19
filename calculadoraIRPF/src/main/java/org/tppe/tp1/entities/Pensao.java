795e15f0c74fe0f3d746992340bdc5283302db2e
lorranyoliveira <lolo.souza80@yahoo.com>
2022-12-18 22:52:43 -0300

    Criacao de classe de Pensao e teste com valor de pensao invalido.
    Co-Authored-By: Bianca Sofia <sofiabrasil44@gmail.com>

---
+++ calculadoraIRPF/src/main/java/org/tppe/tp1/entities/Pensao.java
@@ -0,0 +1,14 @@
+package org.tppe.tp1.entities;
+
+public class Pensao {
+	private double valor;
+	
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+}
---
+++ calculadoraIRPF/src/test/java/org/tppe/tp1/CriaCadastroPensaoTest.java
@@ -0,0 +1,14 @@
+package org.tppe.tp1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CriaCadastroPensaoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
