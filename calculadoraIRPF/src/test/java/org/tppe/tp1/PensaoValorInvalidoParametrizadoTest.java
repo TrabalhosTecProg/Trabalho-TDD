5ee8937de0d291b9643b2e5322d4c989aeab6a93
lorranyoliveira <lolo.souza80@yahoo.com>
2022-12-19 20:18:06 -0300

    Adição de teste parametrizado de valor invalido pensão.
    Co-Authored-By: Bianca Sofia <sofiabrasil44@gmail.com>

---
+++ calculadoraIRPF/src/test/java/org/tppe/tp1/PensaoValorInvalidoParametrizadoTest.java
@@ -0,0 +1,45 @@
+package org.tppe.tp1;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.tppe.tp1.entities.Pensao;
+import org.tppe.tp1.exceptions.ValorPensaoInvalidoException;
+
+@RunWith(Parameterized.class)
+public class PensaoValorInvalidoParametrizadoTest {
+
+	   double valorPensaoInvalido;
+	   private Pensao pensao;
+
+	   public PensaoValorInvalidoParametrizadoTest(double tValor) {
+	       this.valorPensaoInvalido = tValor;
+	   }
+
+	    @Before
+	    public void setup() {
+	        this.pensao = new Pensao();
+	    }
+
+	    @Parameterized.Parameters
+	    public static Collection<Object> getParams() {
+	       Collection <Object> params = new ArrayList<>();
+	       params.add(0.0D);
+	       params.add(-1.0D);
+	       params.add(-0.0D);
+	       params.add(Double.MAX_VALUE);
+
+	       return params;
+	    }
+
+	    @Test(expected = ValorPensaoInvalidoException.class)
+	    public void verificarValorInvalido() throws ValorPensaoInvalidoException {
+	    	pensao.setValor(this.valorPensaoInvalido);
+	    }
+
+}
