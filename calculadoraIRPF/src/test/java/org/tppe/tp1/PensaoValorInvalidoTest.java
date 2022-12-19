d34a7ac4019947da929999f62c06d555b33b4b0e
lorranyoliveira <lolo.souza80@yahoo.com>
2022-12-19 20:00:01 -0300

    Cria teste de pensão valor inválido.
    Co-Authored-By: Bianca Sofia <sofiabrasil44@gmail.com>

---
+++ calculadoraIRPF/src/test/java/org/tppe/tp1/PensaoValorInvalidoTest.java
@@ -0,0 +1,14 @@
+package org.tppe.tp1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PensaoValorInvalidoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
