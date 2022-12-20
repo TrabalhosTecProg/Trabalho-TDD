581c04cdfb46500e27b2cb76386f6a903d8b066a
lorraynecardozo <lorraynecardozo15@gmail.com>
2022-12-20 09:36:34 -0300

    Criação de teste IRPF com falha

---
+++ calculadoraIRPF/src/test/java/org/tppe/tp1/IRPFTest.java
@@ -0,0 +1,13 @@
+package org.tppe.tp1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class IRPFTest {
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
