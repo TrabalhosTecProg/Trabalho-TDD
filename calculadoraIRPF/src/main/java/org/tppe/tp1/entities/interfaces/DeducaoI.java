b7ed1ac33fb604d0f9195677d402182978e284c6
adrian_s_lps <adriansoareslps@gmail.com>
2023-02-08 14:14:05 -0300

    TP3 - Interface para Deduções

--- calculadoraIRPF/src/main/java/org/tppe/tp1/entities/interfaces/DeducaoI.java
+++ calculadoraIRPF/src/main/java/org/tppe/tp1/entities/interfaces/DeducaoI.java
@@ -1,8 +1,15 @@
 package org.tppe.tp1.entities.interfaces;
 
 import org.tppe.tp1.entities.TipoDeducao;
+import org.tppe.tp1.exceptions.DescricaoEmBrancoException;
+import org.tppe.tp1.exceptions.ValorDeducaoInvalidoException;
 
-public interface Deducao {
+public interface DeducaoI {
     public TipoDeducao getType();
     public String getDescricao();
+    public double getValor();
+    public void setDescricao(String descricao) throws DescricaoEmBrancoException;
+    public void setValor(double valor);
+    public void checkValor(double valor) throws ValorDeducaoInvalidoException;
+    Boolean isLimiteValido(Double valor);
 }
